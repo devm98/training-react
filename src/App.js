27012661217e18d815185bd0bd1e042432fe60af
khaminhbui <buidk.minh@gmail.com>
2019-08-19 17:17:51 +0700

    final

--- src/App.js
+++ src/App.js
@@ -8,21 +8,49 @@ export default class App extends Component {
       { name: "Minh", age: 19 },
       { name: "Quí", age: 21 },
       { name: "Tuấn", age: 30 }
-    ]
+    ],
+    showPer: false
   };
   switchNameHandler = () => {
     this.setState({ person: (this.state.persons[0].name = "Thong minh") });
   };
+  changeNameHandler = newName => {
+    this.setState({
+      persons: [
+        { name: newName, age: 19 },
+        { name: newName, age: 21 },
+        { name: newName, age: 30 }
+      ]
+    });
+  };
+  enterNameHandler = event => {
+    this.setState({
+      persons: [
+        { name: event.target.value, age: 19 },
+        { name: "minh", age: 21 },
+        { name: "minh01", age: 30 }
+      ]
+    });
+  };
+  showPersHandler = () => {
+    this.setState({ showPer: !this.state.showPer });
+  };
   render() {
     return (
       <div className="App">
-        {this.state.persons.map(person => (
-          <Person
-            name={person.name}
-            age={person.age}
-            click={this.switchNameHandler}
-          />
-        ))}
+        <button style={{ marginRight: "10px" }} onClick={this.showPersHandler}>
+          {this.state.showPer ? "Hide" : "Show"}
+        </button>
+        {this.state.showPer
+          ? this.state.persons.map(person => (
+              <Person
+                name={person.name}
+                age={person.age}
+                click={() => this.changeNameHandler("Thog minh qua !")}
+                change={this.enterNameHandler}
+              />
+            ))
+          : null}
       </div>
     );
   }
--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -5,6 +5,11 @@ class Person extends React.Component {
   render() {
     return (
       <div onClick={this.props.click} className="person">
+        <input
+          type="text"
+          onChange={this.props.change}
+          value={this.props.name}
+        />
         <h1>{this.props.name}</h1>
         <p>{this.props.age}</p>
         <p>{this.props.children}</p>
